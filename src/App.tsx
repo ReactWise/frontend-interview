f4c7689a5361fde144361e5a62626724fc822815
Seyhan Van Khan <seyhan@reactwise.com>
2025-10-31 14:58:45 +0000

    Continuous Input task

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import ModuleOne from './moduleone';
-const App: React.FC = () => {
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold text-gray-800 mb-4">
-          Frontend Interview Starter
-        </h1>
-        <p className="text-gray-600">
-          This is a minimal React + Vite + Tailwind CSS + TypeScript starter template.
-        </p>
-        <ModuleOne />
-      </div>
-    </div>
-  );
-}
+import { Form } from "./continuous-inputs/form";
 
-export default App;
+export const App = () => {
+	return (
+		<div className="min-h-screen bg-gray-100 flex items-center justify-center">
+			<div className="bg-white p-8 rounded-lg shadow-md">
+				<h1 className="text-2xl font-bold text-gray-800 mb-4">
+					Frontend Interview Starter
+				</h1>
+				<p className="text-gray-600">
+					This is a minimal React + Vite + Tailwind CSS + TypeScript starter
+					template.
+				</p>
+				<Form />
+			</div>
+		</div>
+	);
+};
---
+++ src/hte/moduletwo.tsx
@@ -0,0 +1,14 @@
+export const ModuleTwo = () => {
+	return (
+		<div className="min-h-screen bg-gray-100 flex items-center justify-center">
+			<div className="bg-white p-8 rounded-lg shadow-md">
+				<h1 className="text-2xl font-bold text-gray-800 mb-4">
+					Module Two: Change inner circle red on hover inner/outer circle. Then,
+					show unique information in box when hover. (any, so long as it
+					correlates with highlighted circle)
+				</h1>
+				<img src="/src/assets/moduletwo.png" alt="Module Two" />
+			</div>
+		</div>
+	);
+};
